13f852258b24ebf42e7374e99d890984c4f03e32
Tushar Mehta <tusharmehta08@outlook.com>
2021-07-29 19:48:31 +0530

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+//program for sum of two numbers
+int main()  
+{
+    int a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    cout<<"Sum of two numbers is: "<<a+b;
+    return 0;
+}
+ 
