917d989818d16da2d2fa0d7b1e2bade5d567575c
Tushar Mehta <tusharmehta08@outlook.com>
2021-07-30 20:02:04 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,13 @@
+//program to multiply two numbers
+#include <iostream>
+using namespace std; 
+
+int main() {
+
+    int a,b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "The product of " << a << " and " << b << " is " << a*b << endl;
+    return 0;
+}
+
