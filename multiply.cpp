957390fd57867a0260854a86e0a396a2440bcd4f
Tushar Mehta <tusharmehta08@outlook.com>
2021-08-02 19:29:33 +0530

    corrected sentence in multiply.cpp

--- multiply.cpp
+++ multiply.cpp
@@ -7,7 +7,7 @@ int main() {
     int a,b;
     cout << "Enter two numbers: ";
     cin >> a >> b;
-    cout << "The poduct of " << a << " and " << b << " is " << a*b << endl;
+    cout << "The product of " << a << " and " << b << " is " << a*b << endl;
     return 0;
 }
 
